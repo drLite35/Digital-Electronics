5445efcfefd87701732dc8f19a99abe3ebaf70d5
Anurag Singh <principalquantum30@gmail.com>
2023-09-22 08:43:32 +0530

    Add Verilog code for Half Adder.

---
+++ Verilog/2-Half_add_tb.v
@@ -0,0 +1,18 @@
+module half_add_tb;
+reg a;
+reg b;
+
+wire s;
+wire c;
+
+half_add uut(.a(a),.b(b),.s(s),.c(c));
+
+initial begin
+    $monitor("time=%b , s=%b , c=%b , a=%b , b=%b", $time , s,c,a,b );
+    a=1'b0; b =1'b0;
+    #2 a=1'b0; b =1'b1;
+    #2 a=1'b1; b =1'b0;
+    #2 a=1'b1; b =1'b1;
+    $finish;
+end 
+endmodule
